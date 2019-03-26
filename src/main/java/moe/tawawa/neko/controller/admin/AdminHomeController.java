598eb5c066923fde3e4f1d3ad2414d71a384b118
GeeKaven <xiaokang_show@live.com>
2019-03-27 01:15:25 +0800

    ✨ 后台首页

--- src/main/java/moe/tawawa/neko/controller/admin/AdminHomeController.java
+++ src/main/java/moe/tawawa/neko/controller/admin/AdminHomeController.java
@@ -1,8 +1,21 @@
 package moe.tawawa.neko.controller.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author: GeeKaven
  * @date: 2019/3/21 0:03
  */
+@Controller
+@RequestMapping(value = "/admin")
 public class AdminHomeController {
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("title", "后台管理");
+        return "admin/index";
+    }
 }
---
+++ src/main/java/moe/tawawa/neko/controller/admin/AdminLoginController.java
@@ -0,0 +1,8 @@
+package moe.tawawa.neko.controller.admin;
+
+/**
+ * @author: GeeKaven
+ * @date: 2019/3/25 23:30
+ */
+public class AdminLoginController {
+}
